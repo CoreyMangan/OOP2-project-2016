3bfd1d6d3798bc3c15dfadc3c8d54284b1ef75db
Corey Mangan <t00181698@365s.ittralee.ie>
2016-11-15 15:08:59 +0000

    created main class and finished main jframe

---
+++ MainClass.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainClass {
+	public static void main(String[] args) {
+		MainFrame frame = new MainFrame();
+		frame.setVisible(true);
+	}
+}
--- MainFrame.java
+++ MainFrame.java
@@ -3,22 +3,104 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class MainFrame extends JFrame implements ActionListener{
-	JMenu fileMenu;
+	JMenu fileMenu, gameMenu;
 	JMenuBar menuBar;
 	Container cPane = getContentPane();
 	
+	//Setup JFrame
 	public MainFrame() {
 		super("eGames System");
 		setSize(500,500);
-		setLocation(500,500);
+		setLocation(250,200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//Calling methods
+		createFileMenu();
+		createGameMenu();
 		
 		cPane.setLayout(new FlowLayout());
 		menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
+		menuBar.setBackground(new Color(0xeeddcc));
+		menuBar.add(fileMenu);
+		menuBar.add(gameMenu);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
+		String menuName= e.getActionCommand();
 		
+		//Action When "New" is pressed
+		if(menuName.equals("New")) {
+			JOptionPane.showMessageDialog(null, "New File Created");
+		}
+		
+		//Action When "Save" is pressed
+		if(menuName.equals("Save")) {
+			JOptionPane.showMessageDialog(null, "File Saved");
+		}
+		
+		//Action When "Exit" is pressed
+		if(menuName.equals("Exit")) {
+			JOptionPane.showMessageDialog(null, "Exiting Program... GoodBye!");
+			System.exit(0);
+		}
+	}
+	
+	//Creating File Menu
+	public void createFileMenu() {
+		JMenuItem item;
+		fileMenu = new JMenu("File");
+		
+		//Creating "New" menu item
+		item = new JMenuItem("New");
+		item.addActionListener(this);
+		item.setBackground(new Color(0xccccff));
+		fileMenu.add(item);
+		
+		//Creating "Open" menu item
+		item = new JMenuItem("Open");
+		item.addActionListener(this);
+		item.setBackground(new Color(0xccccff));
+		fileMenu.add(item);
+		
+		//Creating "save" menu item
+		item = new JMenuItem("Save");
+		item.addActionListener(this);
+		item.setBackground(new Color(0xccccff));
+		fileMenu.add(item);
+		
+		fileMenu.addSeparator();
+		
+		//Creating "Exit" menu item
+		item = new JMenuItem("Exit");
+		item.addActionListener(this);
+		item.setBackground(new Color(0xccccff));
+		fileMenu.add(item);
+	}
+	
+	//Creating Game Menu
+	public void createGameMenu() {
+		JMenuItem item;
+		gameMenu = new JMenu("Game");
+		
+		//Creating "Add" menu item
+		item = new JMenuItem("Add");
+		item.addActionListener(this);
+		item.setBackground(new Color(0xccccff));
+		gameMenu.add(item);
+		
+		//Creating "Edit" menu item
+		item = new JMenuItem("Edit");
+		item.addActionListener(this);
+		item.setBackground(new Color(0xccccff));
+		gameMenu.add(item);
+		
+		gameMenu.addSeparator();
+		
+		//Creating "Display" menu item
+		item = new JMenuItem("Display");
+		item.addActionListener(this);
+		item.setBackground(new Color(0xccccff));
+		gameMenu.add(item);
 	}
 }
